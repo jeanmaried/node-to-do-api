8f28f4553c98b10c803e57d1e7fe7092a51525a5
Jean-Marie Dalmasso <jdalmass7@gmail.com>
2019-02-27 21:01:27 +0100

    added env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp", { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI)
 mongoose.set("useCreateIndex", true)
 mongoose.set("useFindAndModify", false)
+mongoose.set("useNewUrlParser", true)
 
 module.exports = { mongoose }
